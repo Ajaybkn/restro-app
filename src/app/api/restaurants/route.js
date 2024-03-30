a943c93eb4d22c29ee0a51a8ff076da3140d9158
Ajay Singh <ajayoas36ty@gmail.com>
2024-03-30 22:05:12 +0530

    MongoDB connection

---
+++ src/app/api/restaurants/route.js
@@ -0,0 +1,11 @@
+import { connectionStr } from "@/app/lib/db";
+import { restaurantSchema } from "@/app/lib/restaurantModel";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    await mongoose.connect(connectionStr, { useNewUrlparser: true })
+    const data = await restaurantSchema.find()
+    console.log(data)
+    return NextResponse.json({ result: data })
+}
---
+++ src/app/lib/db.js
@@ -0,0 +1,2 @@
+const { username, password } = process.env
+export const connectionStr = "mongodb+srv://ajayoas36ty:Google2024@cluster0.8f6dnym.mongodb.net/RestroDB?retryWrites=true&w=majority&appName=Cluster0"
